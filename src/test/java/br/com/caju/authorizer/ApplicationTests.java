26e36832ac1d4e8ec7f3d224b62e42c58391e550
Matheus Maia <mathmferreira@hotmail.com>
2024-08-02 16:33:19 -0300

    create project

---
+++ src/test/java/br/com/caju/authorizer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caju.authorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
