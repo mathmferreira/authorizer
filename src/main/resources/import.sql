7361d8e22d28cd6e3b9c671bbc2d96ba693ed882
Matheus Maia <mathmferreira@hotmail.com>
2024-08-08 15:19:46 -0300

    chore: added inserts for merchant entity

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,6 @@ INSERT INTO account_tb (owner) VALUES ('Alice Johnson'), ('Bob Smith'), ('Carol 
 
 -- Balances
 INSERT INTO benefit_balance_tb (amount, account_id, balance_type) VALUES (600, 1, 'FOOD'), (400, 1, 'MEAL'), (700, 1, 'CASH'),(450, 2, 'FOOD'), (550, 2, 'MEAL'), (400, 2, 'CASH'), (300, 3, 'FOOD'), (700, 3, 'MEAL'), (900, 3, 'CASH'), (400, 4, 'FOOD'), (600, 4, 'MEAL'), (50, 4, 'CASH'), (500, 5, 'FOOD'), (500, 5, 'MEAL'), (150, 5, 'CASH');
+
+-- Merchants
+INSERT INTO merchant_tb (name, balance_type) VALUES ('UBER EATS', 'MEAL'), ('PADARIA DO ZE', 'FOOD'), ('RESTAURANTE DA MARIA', 'MEAL'), ('SUPERMERCADOS BH', 'FOOD');
